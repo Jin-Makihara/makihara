c02dbbaa9ed1e5c0404baa661d2503e5db6bb074
Jin <jackmakijin74@gmail.com>
2023-10-02 01:05:25 +0900

    コメント

--- routes/web.php
+++ routes/web.php
@@ -7,14 +7,14 @@ use App\Http\Controllers\ProductController;
 
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 
-Route::post('/login', [App\Http\Controllers\LoginController ::class, 'login'])->name('login.submit');
+Route::post('/login', [LoginController ::class, 'login'])->name('login.submit');
 
-Route::get('/register', [App\Http\Controllers\RegistrationController::class, 'showRegistrationForm'])->name('register');
-Route::post('/register', [App\Http\Controllers\RegistrationController::class, 'register'])->name('register.submit');
+Route::get('/register', [RegistrationController::class, 'showRegistrationForm'])->name('register');
+Route::post('/register', [RegistrationController::class, 'register'])->name('register.submit');
 
 Route::middleware('auth')->group(function () {
     Route::get('/products', [ProductController::class, 'index'])->name('products');
-    Route::get('/products/create', [App\Http\Controllers\ProductController::class, 'create'])->name('products.create');
+    Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
     Route::post('/products/store', [ProductController::class, 'store'])->name('products.store');
     Route::get('/products/{id}/show', [ProductController::class, 'show'])->name('products.show');
     Route::get('/products/{id}/edit', [ProductController::class, 'edit'])->name('products.edit');

586832606ff6b748dd5d1433ca49de983334f924
Jin <jackmakijin74@gmail.com>
2023-11-29 08:32:53 +0900

    トランザクション入れ込み完了（指摘待ち）

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -20,46 +20,86 @@ class ProductController extends Controller
             $products = $model->searchProduct($searchKeyword, $selectedMaker);
             return view('products.index', compact('products', 'companies'));
         }
-
-        public function create()
+        //登録
+        public function create($id)
         {
+            $product = Product::find($id);
+            DB::beginTransaction();
+            try{
             $create_products = new product;
             $products = $create_products->products();
             $companies = $create_products->companies();
             return view('products.create',compact('products','companies'));
+            DB::commit();
+            }catch(\Exception $e){
+                DB::rollBack();
+                return back();
+            }
         }
 
-        public function store(ProductCreateRequest $request)
+        public function store(ProductCreateRequest $request,$id)
         {
+            $product = Product::find($id);
+            DB::beginTransaction();
+            try{
             // 新規プロダクトの保存
             // メーカー名でCompanyテーブルを検索
             $company = Company::where('company_name', $request->input('company_name'))->first();
             $model = new Product();
             $model->storeProduct($company, $request);
             return redirect()->route('products');
+            DB::commit();
+            }catch(\Exception $e){
+                DB::rollBack();
+                return back();
+            }
         }
 
         public function show($id)
         {
+            $product = Product::find($id);
+            DB::beginTransaction();
+            try{
             $show_product = new Product();
             $product = $show_product->show($id);
             return view('products.show', compact('product'));
+            DB::commit();
+            }catch(\Exception $e){
+                DB::rollBack();
+                return back();
+            }
         }
 
         public function edit($id)
         {
+            $product = Product::find($id);
+            DB::beginTransaction();
+            try{
             $edit_prodcut = new Product();
             $product = $edit_prodcut->edit($id);
             return view('products.edit', compact('product'));
+            DB::commit();
+            }catch(\Exception $e){
+                DB::rollBack();
+                return back();
+            }
         }
-
+        //更新
         public function update(ProductCreateRequest  $request, $id)
         {
+            $product = Product::find($id);
+            DB::beginTransaction();
+            try{
             $model = new Product();
             $model->updateProduct($request,$id);
             return redirect()->route('products')->with('success', '商品が更新されました。');
+            DB::commit();
+            }catch(\Exception $e){
+                DB::rollBack();
+                return back();
+            }
         }
-    
+        //削除
         public function destroy(Request $request, $id)
         {
             $product = Product::find($id);
@@ -73,7 +113,6 @@ class ProductController extends Controller
                 // 商品を削除
                 $product->delete();
                 return redirect()->route('products')->with('success', '商品が削除されました。');
-                $product->delete();
                 DB::commit();
             }catch(\Exception $e){
                 DB::rollback();
